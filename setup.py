948c2a8caa4a62d50b96d22587bc7952016ec2f4
victor <vog1993@gmail.com>
2024-03-06 09:26:47 +0100

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Q&A with Llama and Flask',
+    version= '0.0.0',
+    author= 'Victor Orozco',
+    author_email= 'vog1993@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
